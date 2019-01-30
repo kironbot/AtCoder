982deff4048743bba77149d7852ea7b3514605de
Suguru ARAKI <sgr.araki@gmail.com>
2019-01-30 22:48:40 +0900

    Solve ABC111_A

---
+++ ABC/ABC111/A/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+typedef long long LL;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s[0] == '1') cout << 9;
+    else cout << 1;
+    if (s[1] == '1') cout << 9;
+    else cout << 1;
+    if (s[2] == '1') cout << 9;
+    else cout << 1;
+    cout << endl;
+}
