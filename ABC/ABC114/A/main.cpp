2e2fea35f30120c74e0d0485581e8293377c98e4
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-04 23:18:06 +0900

    Solve ABC114_A

---
+++ ABC/ABC114/A/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+typedef long long LL;
+
+int main() {
+   int x;
+   cin >> x;
+   if (x == 3 || x == 5 || x == 7) puts("YES");
+   else puts("NO");
+}
