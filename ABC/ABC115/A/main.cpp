0689fa06a7fe48d475126f007e4b26a7a0c5429c
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-04 23:20:30 +0900

    Solve ABC115_A

---
+++ ABC/ABC115/A/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+typedef long long LL;
+
+int main() {
+    int d;
+    cin >> d;
+    if (d == 25) puts("Christmas");
+    if (d == 24) puts("Christmas Eve");
+    if (d == 23) puts("Christmas Eve Eve");
+    if (d == 22) puts("Christmas Eve Eve Eve");
+}
