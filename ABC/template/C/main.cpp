0c756fd9ee4601f90d21006294a5e891a33a467c
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-06 23:24:08 +0900

    Update template

--- ABC/template/C/main.cpp
+++ ABC/template/C/main.cpp
@@ -9,8 +9,10 @@
 #include <cmath>
 #include <map>
 using namespace std;
-using Int = long long;
-#define rep(i, n) for (Int i = 0; i < n; i++)
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
 }
