147dc77077d83089e2316a9213aaf5e8cce99479
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-07 19:49:42 +0900

    Solve ABC109/C

--- ABC/ABC109/C/main.cpp
+++ ABC/ABC109/C/main.cpp
@@ -8,8 +8,29 @@
 #include <deque>
 #include <cmath>
 #include <map>
+#include <numeric>
 using namespace std;
-typedef long long LL;
+typedef long long ll;
+
+ll gcd(ll m, ll n) {
+    if (m < n) gcd(n, m);
+    ll r;
+    while (r = m % n) {
+        m = n;
+        n = r;
+    }
+    return n;
+}
 
 int main() {
+    ll n, x;
+    cin >> n >> x;
+    ll ans;
+    for(ll i=0; i<n; i++) {
+        ll tmp;
+        cin >> tmp;
+        if(i==0) ans = abs(tmp - x);
+        else ans = gcd(ans, abs(tmp - x));
+    }
+    cout << ans << endl;
 }
