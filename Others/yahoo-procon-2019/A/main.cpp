b942cfae8d6385abb2a0fe2f1f4697ec42a3c4d9
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-10 20:07:55 +0900

    Solve yahoo-procon-2019/A

---
+++ Others/yahoo-procon-2019/A/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    if (n % 2 == 0) {
+        if (k <= n / 2) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    else {
+        if (k <= (n + 1) / 2) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
