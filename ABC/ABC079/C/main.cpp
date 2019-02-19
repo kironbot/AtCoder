139206f391e6b3189794a53ce8c98ba7195cbe85
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-19 20:46:58 +0900

    Solve ABC079/C

--- ABC/ABC079/C/main.cpp
+++ ABC/ABC079/C/main.cpp
@@ -11,4 +11,26 @@ using ll = long long;
 using namespace std;
 
 int main() {
+    string s;
+    cin >> s;
+    for (int i = 0; i < (1<<3); i++) {
+        int cur = s[0] - '0';
+        string v;
+        v += s[0];
+        for (int j = 1; j < 4; j++) {
+            if ((i >> (j - 1)) & 1) {
+                cur += s[j] - '0';
+                v += "+";
+                v += s[j];
+            } else {
+                cur -= s[j] - '0';
+                v += "-";
+                v += s[j];
+            }
+        }
+        if (cur == 7) {
+            cout << v << "=7" << endl;
+            return 0;
+        }
+    }
 }
