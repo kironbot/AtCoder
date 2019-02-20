982d9aada239cc21eab522a30b12fd2812374461
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 14:55:15 +0900

    Solve AGC015/A

---
+++ AGC/AGC015/A/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n, a, b;
+    cin >> n >> a >> b;
+    if (a > b || (n == 1 && a != b)) cout << 0 << endl; 
+    else cout << b*(n-1)+a - a*(n-1)-b + 1 << endl;
+}
