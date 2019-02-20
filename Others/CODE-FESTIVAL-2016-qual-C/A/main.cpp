837a7aafa017e73bbb5ba99613448c7f30c7e311
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 10:18:05 +0900

    Solve CODE-FESTIVAL-2016-qual-C/A

---
+++ Others/CODE-FESTIVAL-2016-qual-C/A/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    string s;
+    cin >> s;
+    rep(i, s.size()) {
+        for (int j = i+1; j < s.size(); j++) {
+            if (s[i] == 'C' && s[j] == 'F') {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+}
