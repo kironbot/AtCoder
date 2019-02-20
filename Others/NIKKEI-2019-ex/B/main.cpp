320a286ade9a95d90d040b1095f1f9ab00f0ee84
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 20:36:51 +0900

    Solve NIKKEI-2019-ex/B

---
+++ Others/NIKKEI-2019-ex/B/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n;
+    cin >> n;
+    for (ll a = n; a > 0; a--) {
+        if (a * a <= n) {
+            cout << a << endl;
+            return 0;
+        }
+    }
+}
