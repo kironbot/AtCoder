4434f5926ce5a7e91dcc36021ceb3570f81cf5a3
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 11:39:50 +0900

    Solve Tenka1-2016-yosen-B/A

---
+++ Others/Tenka1-2016-yosen-B/A/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+ll fun(ll n) {
+    return (n * n + 4) / 8 ;
+}
+
+int main() {
+    cout << fun(fun(fun(20))) << endl;
+}
