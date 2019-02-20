19a4acf45f08bc9c1bf43fbd57f8015d65837f5b
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 12:49:04 +0900

    Solve CODE-FESTIVAL-2016-qual-C/B

---
+++ Others/CODE-FESTIVAL-2016-qual-C/B/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    int k, t;
+    cin >> k >> t;
+    int amx = 1;
+    rep(i, t) {
+        int a;
+        cin >> a;
+        chmax(amx, a);
+    }
+    cout << max(2*amx - (k+1), 0) << endl;
+}
