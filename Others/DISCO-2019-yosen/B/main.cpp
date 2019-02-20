e5578d665aa9203bcbca1439c45b85ff9ecdbbe2
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 18:01:30 +0900

    Solve DISCO-2018-yosen/B

---
+++ Others/DISCO-2019-yosen/B/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n;
+    cin >> n;
+    if (n % 2 == 0) cout << n*(n/2-1) << endl;
+    else cout << (n-1)*((n-1)/2-1)+1 << endl;
+}
