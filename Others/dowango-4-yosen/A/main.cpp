8105f7bd1bb5c81c6e2aa45f11c719da92bfcbbf
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 09:16:02 +0900

    Solve dowango-4-yosen/A

---
+++ Others/dowango-4-yosen/A/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[0] == s[2] && s[1] == s[3]) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
