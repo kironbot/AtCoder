dc7752bdee46e8b7a620462371e1761d882db316
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 17:11:15 +0900

    Solve CODE-THANKS-FESTIVAL-2018/B

---
+++ Others/CODE-THANKS-FESTIVAL-2018/B/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll x, y;
+    cin >> x >> y;
+    if (3*y-x >=0 && 3*x-y >= 0 && (3*x-y) % 8 == 0) cout << "Yes\n";
+    else cout << "No\n";
+}
