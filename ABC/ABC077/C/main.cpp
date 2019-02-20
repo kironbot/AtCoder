793a7713ea9331d2f15b5e0533a2b3bf16e240b7
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 22:07:11 +0900

    Solve ABC077/C

--- ABC/ABC077/C/main.cpp
+++ ABC/ABC077/C/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 #include <set>
@@ -7,8 +8,28 @@
 #include <deque>
 #include <cmath>
 #include <map>
-using ll = long long;
 using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n), c(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+    rep(i, n) cin >> c[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    sort(c.begin(), c.end());
+
+    ll ans = 0;
+    rep(i, n) {
+        ll cnta = lower_bound(a.begin(), a.end(), b[i]) - a.begin();
+        ll cntc = upper_bound(c.begin(), c.end(), b[i]) - c.begin();
+        ans += cnta * (n - cntc);
+    }
+    cout << ans << endl;
 }
