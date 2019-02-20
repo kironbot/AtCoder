8d06bb541c0d664beb3197e2900aa78b318ce485
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 19:21:26 +0900

    Solve SoundHound-2018-Spring/B

---
+++ Others/SoundHound-2018-Spring/B/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n, l, r;
+    cin >> n >> l >> r;
+    rep(i, n) {
+        ll a;
+        cin >> a;
+        cout << min(max(l, a), r);
+        if (i != n-1) cout << " ";
+    }
+    cout << endl;
+}
