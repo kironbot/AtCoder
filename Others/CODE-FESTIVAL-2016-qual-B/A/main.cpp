b9946b0ea6c6f9fbfdd32dc1ac0063a824125161
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-20 10:17:43 +0900

    Solve CODE-FESTIVAL-2016-qual-B/A

---
+++ Others/CODE-FESTIVAL-2016-qual-B/A/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <map>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    string s, t = "CODEFESTIVAL2016";
+    cin >> s;
+    int ans = 0;
+    rep(i, 16) {
+        if (s[i] != t[i]) ans++;
+    }
+    cout << ans << endl;
+}
