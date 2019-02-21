ddb988584e6cd7e8ed836592b159da0a342e8bfe
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-21 22:39:36 +0900

    Solve ABC076/C

--- ABC/ABC076/C/main.cpp
+++ ABC/ABC076/C/main.cpp
@@ -11,4 +11,24 @@ using ll = long long;
 using namespace std;
 
 int main() {
+    string s, t;
+    cin >> s >> t;
+    ll n = s.size(), m = t.size();
+
+    for (ll i = n-m; i >= 0; i--) {
+        string u = s.substr(i, m);
+        bool ok = true;
+        for (ll j = 0; j < m; j++) {
+            if (u[j] != '?' && u[j] != t[j]) ok = false;
+        }
+        if (ok) {
+            for (ll k = 0; k < n; k++) {
+                if (i <= k && k <= i+m-1) cout << t[k-i];
+                else if (s[k] == '?') cout << 'a';
+                else cout << s[k];
+            }
+            return 0;
+        }
+    }
+    cout << "UNRESTORABLE" << endl;
 }
