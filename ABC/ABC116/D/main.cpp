716629c6c93f9b56265c3c6757be92b43ef02b93
Suguru ARAKI <sgr.araki@gmail.com>
2019-02-22 22:50:46 +0900

    Solve ABC116/D

--- ABC/ABC116/D/main.cpp
+++ ABC/ABC116/D/main.cpp
@@ -9,7 +9,41 @@
 #include <cmath>
 #include <map>
 using namespace std;
-typedef long long LL;
+typedef long long ll;
+const long long INF = 1LL<<40;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> maxd(n, -INF), vals;
+    multiset<ll> se;
+    for (ll i = 0; i < n; i++) {
+        ll s, v;
+        cin >> s >> v;
+        s--;
+        chmax(maxd[s], v);
+        vals.push_back(v);
+    }
+    sort(vals.begin(), vals.end(), greater<ll>());
+    sort(maxd.begin(), maxd.end(), greater<ll>());
+    ll sum = 0;
+    for (ll i = 0; i < k; i++) sum += vals[i], se.insert(vals[i]);
+
+    ll ans = 0, tmp = sum;
+    for (ll x = 1; x <= k; x++) {
+        ll add = maxd[x-1];
+        ll back = *se.begin();
+        tmp += add;
+        if (back > add) {
+            se.erase(se.begin());    
+            tmp -= back;
+        } else {
+            se.erase(se.find(add));
+            tmp -= add;
+        }
+        chmax(ans, tmp + x * x);
+    }
+    cout << ans << endl;
 }
