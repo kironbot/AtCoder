4589de42c9eb9c696cea33c077655b872f65328e
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-02 22:27:16 +0900

    Solve ABC110/D

--- ABC/ABC110/D/main.cpp
+++ ABC/ABC110/D/main.cpp
@@ -1,15 +1,56 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long LL;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+// 素因数分解を求めてベクトル化するライブラリ
+vector<pair<long long, long long> > prime_factorize(long long n) {
+    vector<pair<long long, long long> > res;
+    for (long long p = 2; p * p <= n; ++p) {
+        if (n % p != 0) continue;
+        int num = 0;
+        while (n % p == 0) { ++num; n /= p; }
+        res.push_back(make_pair(p, num));
+    }
+    if (n != 1) res.push_back(make_pair(n, 1));
+    return res;
+}
+
+// コンビネーションを求めるライブラリ
+const int MAX = 210000;
+long long fac[MAX], finv[MAX], inv[MAX];
+void COMinit(){
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for(int i = 2; i < MAX; i++){
+        fac[i] = fac[i-1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD/i) % MOD;
+        finv[i] = finv[i-1] * inv[i] % MOD;
+    }
+}
+
+long long COM(int n, int k){
+    if(n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD;
+}
 
 int main() {
+    int n, m;
+    COMinit();
+    cin >> n >> m;
+    auto vec = prime_factorize(m);
+    ll ans = 1;
+    for (auto pa : vec) {
+        ll num = pa.second;
+        ll tmp = COM(num+n-1, n-1);
+        ans = (ans * tmp) % MOD;
+    }
+    cout << ans << endl;
 }
