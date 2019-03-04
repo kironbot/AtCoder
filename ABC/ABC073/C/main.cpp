3824fcbc54f64879759449a5a761a8b83db553ba
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-04 22:57:40 +0900

    Solve ABC073/C

--- ABC/ABC073/C/main.cpp
+++ ABC/ABC073/C/main.cpp
@@ -1,14 +1,22 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
-using ll = long long;
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n;
+    cin >> n;
+    set<ll> s;
+    rep(i, 0, n) {
+        ll a;
+        cin >> a;
+        if (s.find(a) == s.end()) s.insert(a);
+        else s.erase(a);
+    }
+    cout << s.size() << endl;
 }
