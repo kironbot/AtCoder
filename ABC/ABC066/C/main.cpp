45d646b08750ec0f3d3bc19afd5875a8ead89357
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-05 22:02:46 +0900

    Solve ABC066/C

--- ABC/ABC066/C/main.cpp
+++ ABC/ABC066/C/main.cpp
@@ -1,14 +1,26 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
-using ll = long long;
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n;
+    cin >> n;
+    deque<ll> dq;
+    rep(i, 0, n) {
+        ll a;
+        cin >> a;
+        if (i % 2 == 0) dq.push_front(a);
+        else dq.push_back(a);
+    }
+    if (n % 2 == 0) reverse(dq.begin(), dq.end());
+    rep(i, 0, n) {
+        cout << dq[i] << " ";
+    }
+    cout << endl;
 }
