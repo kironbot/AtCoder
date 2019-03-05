f042f59c64e92da63fded7cceba5b08f2c83269b
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-05 22:15:53 +0900

    Solve ABC065/C

--- ABC/ABC065/C/main.cpp
+++ ABC/ABC065/C/main.cpp
@@ -1,14 +1,22 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
-using ll = long long;
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n, m;
+    cin >> n >> m;
+    ll MAX = max(n, m);
+    ll memo[MAX];
+    memo[1] = 1;
+    rep(i, 2, MAX+1) memo[i] = (memo[i-1] * i) % MOD;
+
+    if (abs(n-m) > 2) cout << 0 << endl;
+    else if (abs(n-m) == 1) cout << memo[n] * memo[m] % MOD << endl;
+    else cout << 2 * memo[n] * memo[m] % MOD << endl;
 }
