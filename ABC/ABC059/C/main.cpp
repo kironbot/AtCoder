72849d3c63dd0ca183e4973eee2f31a9b982b4bf
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-06 22:39:15 +0900

    Solve ABC059/C

--- ABC/ABC059/C/main.cpp
+++ ABC/ABC059/C/main.cpp
@@ -1,14 +1,29 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
-using ll = long long;
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i, 0, n) cin >> a[i];
+
+    ll sum1 = 0, sum2 = 0, tmp1 = 0, tmp2 = 0;
+    rep(i, 0, n) {
+        sum1 += a[i], sum2 += a[i];
+        if (i % 2 == 0) {
+            if (sum1 <= 0) tmp1 += abs(sum1) + 1, sum1 = 1;
+            if (sum2 >= 0) tmp2 += abs(sum2) + 1, sum2 = -1;
+        } else {
+            if (sum1 >= 0) tmp1 += abs(sum1) + 1, sum1 = -1;
+            if (sum2 <= 0) tmp2 += abs(sum2) + 1, sum2 = 1;
+        }
+    }
+    cout << min(tmp1, tmp2) << endl;
 }
