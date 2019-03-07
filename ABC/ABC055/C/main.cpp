955bac4abbdcff4d34d8d5408b9ce326ff5cbeac
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-07 21:13:02 +0900

    Solve ABC055/C

--- ABC/ABC055/C/main.cpp
+++ ABC/ABC055/C/main.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
-using ll = long long;
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n, m;
+    cin >> n >> m;
+    cout << min(n, m/2) + max(m-2*n, (ll)0) / 4 << endl;
 }
