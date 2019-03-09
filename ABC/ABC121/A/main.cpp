e65cbbf9008bcd48fb14737f2804faefddef0923
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-09 23:14:25 +0900

    Solve ABC121/A

---
+++ ABC/ABC121/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll H, W, h, w;
+    cin >> H >> W >> h >> w;
+    cout << H*W - h*W - H*w + h*w << endl;
+}
