d06f24f51ca3283125ea429de625df1e9f39d3b9
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-10 16:53:58 +0900

    Solve AGC020/A

---
+++ AGC/AGC020/A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n, a, b;
+    cin >> n >> a >> b;
+    if ((b-a) % 2 == 1) cout << "Borys" << endl;
+    else cout << "Alice" << endl;
+}
