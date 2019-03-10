04a1f7ff9019830f1c17c9e11f3b513cf1199119
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-10 17:01:48 +0900

    Solve AGC005/A

---
+++ AGC/AGC005/A/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    string x;
+    cin >> x;
+    ll ans = (ll)x.size();
+    deque<char> dq;
+    for (char c : x) {
+        if (!dq.empty() && dq.back() == 'S' && c == 'T') dq.pop_back(), ans-=2;
+        else dq.push_back(c);
+    }
+    cout << ans << endl;
+}
