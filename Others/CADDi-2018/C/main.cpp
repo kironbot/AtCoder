994768e2e08cf5eee36c202a9c10fd4b417bf951
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-12 22:54:13 +0900

    Solve CADDi-2018/C

---
+++ Others/CADDi-2018/C/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+vector<pair<long long, long long> > prime_factorize(long long n) {
+    vector<pair<long long, long long> > res;
+    for (long long p = 2; p * p <= n; ++p) {
+        if (n % p != 0) continue;
+        int num = 0;
+        while (n % p == 0) { ++num; n /= p; }
+        res.push_back(make_pair(p, num));
+    }
+    if (n != 1) res.push_back(make_pair(n, 1));
+    return res;
+}
+
+int main() {
+    ll n, p;
+    cin >> n >> p;
+    auto vp = prime_factorize(p);
+    ll ans = 1;
+    for (auto pa : vp) {
+        if (pa.second >= n) {
+            ll e = pa.second / n;
+            rep(i, 0, e) ans *= pa.first;
+        }
+    }
+    cout << ans << endl;
+}
