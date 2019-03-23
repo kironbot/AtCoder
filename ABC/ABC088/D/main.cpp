07aa2a64a49518a70fcb2b4391fa8f05227c7450
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-23 21:43:14 +0900

    Solve ABC088/D

--- ABC/ABC088/D/main.cpp
+++ ABC/ABC088/D/main.cpp
@@ -8,43 +8,39 @@ const ll INF = 1LL << 60;
 template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
 template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
-ll W, H;
-char mas[500][500];
-ll min_cost[500][500];
+int main() {
+    ll dx[4] = {1, 0, -1, 0};
+    ll dy[4] = {0, 1, 0, -1};
+    ll H, W;
+    string fi[110];
+    ll dist[110][110];
 
-ll  bfs(ll sx, ll sy, char c) {
-    const ll vx[] = {0, 1, 0, -1}, vy[] = {1, 0, -1, 0};
-    memset(min_cost, -1, sizeof(min_cost));
+    cin >> H >> W;
+    ll wnum = 0;
+    rep(i, 0, H) {
+        cin >> fi[i];
+        rep(j, 0, W) if (fi[i][j] == '.') wnum++;
+    }
 
+    memset(dist, -1, sizeof(dist));
+    dist[0][0] = 1;
     queue<pair<ll, ll>> que;
-    que.emplace(sx, sy); 
-    min_cost[sx][sy] = 0;
-
+    que.push(pair<ll,ll>(0, 0));
     while(!que.empty()) {
-        auto p = que.front();
+        auto cur = que.front();
         que.pop();
-        if (mas[p.second][p.first] == c) return (min_cost[p.first][p.second]);
-        rep(i, 0, 4) {
-            ll nx = p.first + vx[i], ny = p.second + vy[i];
-            if(nx < 0 || ny < 0 || nx >= W || ny >= H) continue;
-            if(min_cost[nx][ny] != -1) continue;
-            if(mas[nx][ny] == '#') continue;
-            min_cost[nx][ny] = min_cost[p.first][p.second] + 1;
-            que.emplace(nx, ny);
+        ll x = cur.first, y = cur.second;
+        rep(dir, 0, 4) {
+            ll nx = x + dx[dir];
+            ll ny = y + dy[dir];
+            if(nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+            if(fi[nx][ny] == '#') continue;
+            if(dist[nx][ny] == -1) {
+                dist[nx][ny] = dist[x][y] + 1;
+                que.push(pair<ll, ll>(nx, ny));
+            }
         }
     }
-    return -1;
-}
-
-int main() {
-    cin >> H >> W;
-    ll proc = 0;
-    rep(i, 0, H) {
-        cin >> mas[i];
-        rep(j, 0, W) if(mas[i][j] == '#') proc++;
-    }
-    mas[H-1][W-1] = 'G';
-    auto s = bfs(0, 0, 'G');
-    if (s == -1) cout << -1 << endl;
-    else cout << H * W - s - proc - 1 << endl;
+    if (dist[H-1][W-1] == -1) cout << -1 << endl;
+    else cout << wnum - dist[H-1][W-1] << endl;
 }
