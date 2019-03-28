9594b1647d2ce607778712f3b9bedbbe96d283c0
Suguru ARAKI <sgr.araki@gmail.com>
2019-03-28 22:20:46 +0900

    Solve ABC042/D

--- ABC/ABC042/D/main.cpp
+++ ABC/ABC042/D/main.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <map>
-using ll = long long;
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+const int MAX = 510000;
+long long fac[MAX], finv[MAX], inv[MAX];
+void COMinit(){
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
 
 int main() {
+    COMinit();
+    ll h, w, a, b;
+    cin >> h >> w >> a >> b;
+    ll ans = 0;
+    rep(i, b, w+1) {
+        ans += COM(h-a-1+i, i) * COM(a+w-2-i, a-1) % MOD; 
+    }
+    ans %= MOD;
+    cout << ans << endl;
 }
