cf2a41a89fdbf2d64cd0058b4dff94f2e048cd7d
Suguru ARAKI <sgr.araki@gmail.com>
2019-04-06 23:22:26 +0900

    Solve ABC123/A

---
+++ ABC/ABC123/A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    vector<ll> v(5);
+    rep(i, 0, 5) cin >> v[i];
+    ll k;
+    cin >> k;
+    if (v[4] - v[0] > k) cout << ":(" << endl;
+    else cout << "Yay!" << endl;
+}
