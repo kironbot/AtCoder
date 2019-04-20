30dcc7b7133e75fbe21432f74129dc3c326f2f92
Suguru ARAKI <sgr.araki@gmail.com>
2019-04-20 21:39:56 +0900

    Solve Tenka1-PBC-2019/B

---
+++ Others/Tenka1-PBC-2019/B/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n, k;
+    string s;
+    cin >> n >> s >> k;
+    for(auto c : s) {
+        if (c != s[k-1]) cout << '*';
+        else cout << s[k-1];
+    }
+    cout << endl;
+}
