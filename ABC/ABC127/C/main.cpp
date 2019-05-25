58f86ee4e2b182a191a5a9a2c793cffe76b1e2ae
Suguru ARAKI <sgr.araki@gmail.com>
2019-05-25 22:50:16 +0900

    Solve ABC127/C

---
+++ ABC/ABC127/C/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    ll left = -INF, right = INF;
+    rep(i, 0, m) {
+        ll l, r;
+        cin >> l >> r;
+        chmax(left, l);
+        chmin(right, r);
+    } 
+    cout << max(0LL, right-left+1) << endl;
+}
