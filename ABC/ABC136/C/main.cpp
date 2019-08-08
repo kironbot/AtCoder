e65e0fc21fe186b712d8eaea561631097031a7c3
Suguru ARAKI <sgr.araki@gmail.com>
2019-08-08 18:06:20 +0900

    Solve ABC136/C

--- ABC/ABC136/C/main.cpp
+++ ABC/ABC136/C/main.cpp
@@ -9,4 +9,23 @@ template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
 template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
 int main() {
+    ll n;
+    cin >> n;
+    vector<ll> h(n);
+    rep(i, 0, n) cin >> h[i];
+
+    bool ok = true;
+    rep(i, 0, n) {
+        if(i == 0) {
+            h[i]--;
+            continue;
+        }
+        if (h[i] < h[i-1]) {
+            ok = false;
+            break;
+        }
+        if (h[i]-1 >= h[i-1]) h[i]--;
+    }
+    if(ok) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
