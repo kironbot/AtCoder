d4899b93408c3c48489c746b7755c2648682d864
Suguru ARAKI <sgr.araki@gmail.com>
2019-08-08 17:59:39 +0900

    Solve ABC136/B

--- ABC/ABC136/B/main.cpp
+++ ABC/ABC136/B/main.cpp
@@ -8,5 +8,20 @@ const ll INF = 1LL << 60;
 template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
 template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
 
+ll count_digit(ll n) {
+    ll res = 0;
+    while(n) {
+        n /= 10;
+        res++;
+    }
+    return res;
+}
+
+
 int main() {
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    rep(i, 1, n+1) if(count_digit(i) % 2 == 1) ans++;
+    cout << ans << endl;
 }
