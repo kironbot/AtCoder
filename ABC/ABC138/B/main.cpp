106229a0c3813ce2b31b5198dab86687e05f1ba5
Suguru ARAKI <sgr.araki@gmail.com>
2019-08-18 22:41:37 +0900

    Solve ABC138/B

---
+++ ABC/ABC138/B/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n;
+    cin >> n;
+    double x;
+    rep(i, 0, n) {
+        double a;
+        cin >> a;
+        x += 1.0 / a;
+    }
+    cout.precision(10);
+    cout << 1.0 / x << endl;
+}
