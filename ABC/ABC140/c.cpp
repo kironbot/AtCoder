80e7800907e81bbba8f2bdced7b784ff250bb795
Suguru ARAKI <sgr.araki@gmail.com>
2019-09-28 21:00:39 +0900

    Solve ABC140/C

---
+++ ABC/ABC140/c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = 1LL << 60;
+#define rep(i, a, n) for (ll i = (a); i < (n); i++)
+#define debug(x) cerr << #x << ": " << x << endl;
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> b(n-1);
+    rep(i, 0, n-1) cin >> b[i];
+    ll ans = b[0] + b[n-2];
+    rep(i, 0, n-2) ans += min(b[i], b[i+1]);
+    cout << ans << endl;
+}
